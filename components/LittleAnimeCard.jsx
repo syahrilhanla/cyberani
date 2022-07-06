e468d13d2d293373b4de3dbe97fe967e20c27ec4
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-06 17:01:36 +0800

    added title to LittleAnimeCard

--- components/LittleAnimeCard.jsx
+++ components/LittleAnimeCard.jsx
@@ -4,10 +4,18 @@ import { FaPlay } from "react-icons/fa";
 const LittleAnimeCard = () => {
 	return (
 		<div>
-			<div className="relative ">
-				<div className="absolute top-0 left-0 z-10 h-full w-full">
-					<div className="flex items-center absolute top-16 left-3 gap-2">
-						<FaPlay /> <p>S1 Ep1</p>
+			<div className="relative group">
+				<div className="absolute top-0 left-0 z-10 h-full w-full opacity-10 group-hover:opacity-100 duration-300">
+					<div className="flex items-center">
+						<span className="relative left-1 top-1 text-white px-1 ">
+							<div className="absolute -z-10 top-1 bg-slate-900 opacity-60 h-full w-full"></div>
+							<p className="opacity-100 z-50 text-sm font-normal">
+								Jujutsu Kaisen Zero One Two Three
+							</p>
+						</span>
+						<span className="flex items-center absolute top-16 left-3 gap-2">
+							<FaPlay /> <p>S1 Ep1</p>
+						</span>
 					</div>
 				</div>
 				<span>
