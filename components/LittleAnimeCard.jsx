9b9b578882d0b234a186689fb69ddd945524a1e3
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-06 17:36:33 +0800

    text-ellipsis for title in LitleAnimeCard

--- components/LittleAnimeCard.jsx
+++ components/LittleAnimeCard.jsx
@@ -8,12 +8,12 @@ const LittleAnimeCard = () => {
 				<div className="absolute top-0 left-0 z-10 h-full w-full opacity-10 group-hover:opacity-100 duration-300">
 					<div className="flex w-fit">
 						<span className="relative left-1 top-1 text-white px-1 ">
-							<div className="absolute -z-10 top-1 bg-slate-900 opacity-60 h-full w-full"></div>
-							<p className="opacity-100 z-50 text-sm font-normal">
-								Jujutsu Kaisen Zero
+							<div className="absolute -z-10 top-1 bg-black opacity-100 h-full w-fit"></div>
+							<p className="opacity-100 z-50 w-[80%] text-sm font-normal overflow-hidden text-ellipsis whitespace-nowrap">
+								Jujutsu Kaisen Zero Kaisen Zero
 							</p>
 						</span>
-						<span className="flex items-center absolute top-16 left-3 gap-2">
+						<span className="flex items-center absolute top-[65%] left-3 gap-2">
 							<FaPlay /> <p>S1 Ep1</p>
 						</span>
 					</div>
