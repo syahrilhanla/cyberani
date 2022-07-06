7b990024bd2968a9bcd9b14c8f7e4a4f6a54d0c0
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-06 17:15:45 +0800

    added carousel to AnimeRow

--- components/AnimeRow.jsx
+++ components/AnimeRow.jsx
@@ -1,17 +1,76 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+
 import LittleAnimeCard from "./LittleAnimeCard";
 
 const AnimeRow = ({ rowTitle }) => {
 	return (
 		<div className="flex flex-col w-[90%] mt-2 text-left font-medium text-slate-200">
 			<h1 className="text-2xl mb-2">{rowTitle}</h1>
-			<div className="grid grid-cols-6 gap-3">
-				<LittleAnimeCard />
-				<LittleAnimeCard />
-				<LittleAnimeCard />
-				<LittleAnimeCard />
-				<LittleAnimeCard />
-				<LittleAnimeCard />
+			<div className="w-full">
+				<Swiper
+					// install Swiper modules
+					slidesPerView={6}
+					modules={[Navigation]}
+					navigation={{
+						nextEl: ".swiper-button-next",
+						prevEl: ".swiper-button-prev",
+					}}
+					spaceBetween={10}
+				>
+					<SwiperSlide>
+						<LittleAnimeCard />
+					</SwiperSlide>
+					<SwiperSlide>
+						<LittleAnimeCard />
+					</SwiperSlide>
+					<SwiperSlide>
+						<LittleAnimeCard />
+					</SwiperSlide>
+					<SwiperSlide>
+						<LittleAnimeCard />
+					</SwiperSlide>
+					<SwiperSlide>
+						<LittleAnimeCard />
+					</SwiperSlide>
+					<SwiperSlide>
+						<LittleAnimeCard />
+					</SwiperSlide>
+					<SwiperSlide>
+						<LittleAnimeCard />
+					</SwiperSlide>
+					<SwiperSlide>
+						<LittleAnimeCard />
+					</SwiperSlide>
+					<SwiperSlide>
+						<LittleAnimeCard />
+					</SwiperSlide>
+					<SwiperSlide>
+						<LittleAnimeCard />
+					</SwiperSlide>
+					<SwiperSlide>
+						<LittleAnimeCard />
+					</SwiperSlide>
+					<div
+						className="swiper-button-next opacity-40 hover:opacity-100 duration-200 mr-[-0.5rem]"
+						style={{ color: "#c5c5c5" }}
+					></div>
+					<div
+						className="swiper-button-prev opacity-40 hover:opacity-100 duration-200 ml-[-0.5rem]"
+						style={{ color: "#c5c5c5" }}
+					></div>
+				</Swiper>
 			</div>
+			{/* <div className="grid grid-cols-3 md:grid-cols-6 gap-3">
+				<LittleAnimeCard />
+				<LittleAnimeCard />
+				<LittleAnimeCard />
+				<LittleAnimeCard />
+				<LittleAnimeCard />
+				<LittleAnimeCard />
+			</div> */}
 		</div>
 	);
 };
--- components/LittleAnimeCard.jsx
+++ components/LittleAnimeCard.jsx
@@ -6,11 +6,11 @@ const LittleAnimeCard = () => {
 		<div>
 			<div className="relative group">
 				<div className="absolute top-0 left-0 z-10 h-full w-full opacity-10 group-hover:opacity-100 duration-300">
-					<div className="flex items-center">
+					<div className="flex w-fit">
 						<span className="relative left-1 top-1 text-white px-1 ">
 							<div className="absolute -z-10 top-1 bg-slate-900 opacity-60 h-full w-full"></div>
 							<p className="opacity-100 z-50 text-sm font-normal">
-								Jujutsu Kaisen Zero One Two Three
+								Jujutsu Kaisen Zero
 							</p>
 						</span>
 						<span className="flex items-center absolute top-16 left-3 gap-2">
