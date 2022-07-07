2b2748349171f459bb7e7d080e12eac36b176708
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-07 14:38:53 +0800

    migrate to Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
-import Head from "next/head";
 import Home from "../components/Home";
-import Navbar from "../components/Navbar";
+import Layout from "../components/Layout";
 
 export default function HomePage() {
 	return (
 		<div>
-			<Head>
-				<title>CyberAni - Watch Free Anime</title>
-				<meta name="description" content="Free streaming anime platform" />
-				<link rel="icon" href="/CyberAni-logo.png" />
-			</Head>
-			<Navbar />
-			<Home />
+			<Layout>
+				<Home />
+			</Layout>
 		</div>
 	);
 }
---
+++ pages/tryout.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+
+const tryout = () => {
+	return (
+		<div>
+			<Layout>
+				<h1 className="font-light text-2xl h-[100vh] flex items-center justify-center text-slate-300">
+					This is a page for trying out components
+				</h1>
+			</Layout>
+		</div>
+	);
+};
+
+export default tryout;
