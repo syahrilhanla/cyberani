2614b3f03f86db7145b11017e161064a80a881c7
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-07 08:16:33 +0800

    adjust Jumbotron

--- components/Jumbotron.jsx
+++ components/Jumbotron.jsx
@@ -1,14 +1,8 @@
 import Image from "next/image";
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
 const Jumbotron = ({ data }) => {
 	return (
 		<div className=" text-gray-300 flex justify-center min-w-full px-8">
-			{/* <span className="w-[50px] flex items-center justify-start duration-300 opacity-30 group hover:opacity-100">
-				<div>
-					<FaArrowLeft className="w-6 h-6" />
-				</div>
-			</span> */}
 			<div className="flex md:flex-row flex-col justify-center items-center bg-[#030B17] rounded-lg">
 				<div className="px-4 max-w-md mb-5 md:mb-0 w-[300px] md:w-full min-h-[268px]">
 					<h2 className="text-3xl font-semibold mb-3 md:mt-0 mt-6 text-gray-200">
@@ -25,7 +19,6 @@ const Jumbotron = ({ data }) => {
 					<div
 						className="absolute top-0 left-0 z-10 w-full h-full 
 						shadow-black shadow-2xl box-shadow opacity-100"
-						// style={{ boxShadow: "130px -1px 60px 0px #030B17 inset" }}
 					></div>
 					<div className="z-0 min-w-[200px] min-h-[110px]">
 						<Image
@@ -38,11 +31,6 @@ const Jumbotron = ({ data }) => {
 					</div>
 				</div>
 			</div>
-			{/* <span className="w-[50px] flex items-center justify-end duration-300 opacity-30 group hover:opacity-100">
-				<div>
-					<FaArrowRight className="w-6 h-6" />
-				</div>
-			</span> */}
 		</div>
 	);
 };
---
+++ pages/tryout.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+const tryout = () => {
+	return <div>Fetching anime</div>;
+};
+
+export default tryout;
