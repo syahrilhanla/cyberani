dd2f24b9490dda81e8c96a26a4f9631e4869004e
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-07 08:41:31 +0800

    added gogocdn.net to image domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: ["gogocdn.net"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
