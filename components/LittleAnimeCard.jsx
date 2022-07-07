821c21f79a46e88ddf0d9b5dd2475540e28d0eca
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-07 09:40:36 +0800

    adjusted title text for AnimeCard mobile layout

--- components/LittleAnimeCard.jsx
+++ components/LittleAnimeCard.jsx
@@ -5,14 +5,11 @@ const LittleAnimeCard = () => {
 	return (
 		<div>
 			<div className="relative group">
-				<div className="absolute top-0 left-0 z-10 h-full w-full opacity-20 group-hover:opacity-100 duration-300 box-shadow-little">
-					<div className="flex w-fit shadow-inner ">
-						<span className="relative left-1 top-1 text-white px-1 ">
-							<div className="absolute -z-10 top-1 bg-black opacity-100 h-full w-fit"></div>
-						</span>
-						<span className="flex flex-col absolute top-[75%] left-3">
-							<p className="opacity-100 z-50 w-[80%] text-sm font-normal overflow-hidden text-ellipsis whitespace-nowrap">
-								Jujutsu Kaisen Zero Kaisen Zero
+				<div className="absolute top-0 left-0 z-10 h-[100%] w-full rounded-lg opacity-20 group-hover:opacity-100 duration-300 box-shadow-little flex items-end">
+					<div className=" absolute left-3 md:left-3 bottom-3">
+						<span className="flex flex-col md:w-[75%]">
+							<p className="opacity-100 z-50 text-sm font-normal md:truncate">
+								Jujutsu Kaisen Zero One Two Three
 							</p>
 							<span className="flex items-center left-3 gap-2">
 								<FaPlay /> <p>S1 Ep1</p>
@@ -21,13 +18,9 @@ const LittleAnimeCard = () => {
 					</div>
 				</div>
 				<span>
-					<Image
-						src={
-							"https://gogocdn.net/cover/shin-tennis-no-ouji-sama-u-17-world-cup.png"
-						}
-						width={200}
-						height={280}
-						className="rounded-lg z-0"
+					<img
+						src="https://gogocdn.net/cover/shin-tennis-no-ouji-sama-u-17-world-cup.png"
+						className="object-cover w-full h-full rounded-xl"
 					/>
 				</span>
 			</div>
