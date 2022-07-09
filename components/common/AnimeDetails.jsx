4762996abf9d836bcd0eb4ca98d6598192e81982
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-09 08:42:23 +0800

    set anime current episode

--- components/common/AnimeDetails.jsx
+++ components/common/AnimeDetails.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import AnimeInfo from "../anime/AnimeInfo";
 import Episodes from "../anime/Episodes";
 import VideoComponent from "../anime/VideoComponent";
 
 const AnimeDetails = ({ animeData }) => {
+	const lastIndexOfAnime =
+		animeData.episodesList &&
+		animeData.episodesList[animeData.episodesList.length - 1].episodeNum;
+	const [currentEpisode, setCurrentEpisode] = useState(lastIndexOfAnime);
+	const [episodeDetail, setEpisodeDetail] = useState(
+		animeData.episodesList && animeData.episodesList[0]
+	);
+
 	return (
 		<div
 			className="grid md:grid-cols-[3fr_5fr_2fr] w-full h-fit
      mx-auto px-4 md:px-14 gap-12 justify-between mt-5"
 		>
 			<AnimeInfo animeData={animeData} />
-			<VideoComponent />
-			<Episodes animeData={animeData} />
+			<VideoComponent
+				title={`${animeData.animeTitle} - Episode ${currentEpisode}`}
+				episodeDetail={episodeDetail}
+			/>
+			<Episodes
+				animeData={animeData}
+				currentEpisode={currentEpisode}
+				setCurrentEpisode={setCurrentEpisode}
+			/>
 		</div>
 	);
 };
