52ae09d3898b0226f0c73e92718eb5f66527a268
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-09 08:42:08 +0800

    play firts index of current title

--- components/anime/VideoComponent.jsx
+++ components/anime/VideoComponent.jsx
@@ -1,12 +1,32 @@
-const VideoComponent = () => {
+import { useEffect, useState } from "react";
+import { fetchAnimeEpisode } from "../utils/fetchAnime";
+
+const VideoComponent = ({ title, episodeDetail }) => {
+	const [episodeURL, setEpisodeURL] = useState();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const results = await fetchAnimeEpisode(episodeDetail.episodeId);
+			setEpisodeURL(results);
+			console.log(episodeURL.Referer);
+		};
+
+		episodeDetail && fetchData();
+	}, [episodeDetail]);
+
 	return (
-		<iframe
-			allowFullScreen={true}
-			width="100%"
-			height="100%"
-			scrolling="no"
-			src={`https://goload.pro/streaming.php?id=MTg4Nzg4&title=Isekai+Meikyuu+de+Harem+wo+Episode+1`}
-		></iframe>
+		<div>
+			<h1 className="text-xl font-semibold text-slate-200">{title}</h1>
+			{episodeURL.Referer && (
+				<iframe
+					allowFullScreen={true}
+					width="100%"
+					height="100%"
+					scrolling="no"
+					src={`${episodeURL.Referer}`}
+				></iframe>
+			)}
+		</div>
 	);
 };
 
