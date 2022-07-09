06b17b3f32f7b3aa403db9e08850b4c074fa0ffd
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-09 08:41:06 +0800

    added conditional rendering

--- components/anime/Episodes.jsx
+++ components/anime/Episodes.jsx
@@ -1,18 +1,20 @@
-const Episodes = ({ animeData }) => {
+const Episodes = ({ animeData, setCurrentEpisode }) => {
 	return (
 		<div className="text-slate-200 text-left mt-3">
 			<h1 className="text-3xl font-semibold mb-3">Episodes</h1>
 			<div className="py-0">
-				{animeData.episodesList.reverse().map((episode) => {
-					return (
-						<h4
-							className="text-lg font-normal py-2 ml-2"
-							key={episode.episodeNum}
-						>
-							Episode {episode.episodeNum}
-						</h4>
-					);
-				})}
+				{animeData.episodesList &&
+					animeData.episodesList.reverse().map((episode) => {
+						return (
+							<h4
+								className="text-lg font-normal py-2 ml-2 cursor-pointer"
+								key={episode.episodeNum}
+								onClick={() => setCurrentEpisode(episode.episodeNum)}
+							>
+								Episode {episode.episodeNum}
+							</h4>
+						);
+					})}
 			</div>
 		</div>
 	);
