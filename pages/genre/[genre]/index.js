4afd422669143392014cea3f5894f9c673e6f21d
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-12 11:38:41 +0800

    capitalized genre word for title

--- pages/genre/[genre]/index.js
+++ pages/genre/[genre]/index.js
@@ -8,6 +8,13 @@ const index = ({ genre }) => {
 	const [currentPage, setCurrentPage] = useState(1);
 	const [animeData, setAnimeData] = useState([]);
 
+	const capitalizedWord = (word) => {
+		const firstLetter = word.split("")[0].toUpperCase();
+		const newWord = word.split("");
+		newWord[0] = firstLetter;
+		return newWord.join("");
+	};
+
 	const fetchData = async () => {
 		const results = await fetchAnimeGenre(genre, currentPage);
 		setAnimeData(results);
@@ -20,13 +27,13 @@ const index = ({ genre }) => {
 	return (
 		<>
 			<Meta
-				title={`${genre} - CyberAni`}
+				title={`${capitalizedWord(genre)} - CyberAni`}
 				description={"Watch the latest released anime here for free!"}
 			/>
 			<div className="w-[90%] md:w-full p-3 my-6 flex flex-col justify-center items-center">
 				<div className="md:w-[70%] ml-3 mb-4">
 					<h1 className="text-left text-slate-200 text-3xl font-semibold">
-						{genre}
+						{capitalizedWord(genre)}
 					</h1>
 				</div>
 				<div
