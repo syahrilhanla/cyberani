fc98ac2a95c8f1b1a4c5d2274de2e3a0def39b6d
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-12 10:29:54 +0800

    display none if nothing was found

--- components/common/DisplayResults.jsx
+++ components/common/DisplayResults.jsx
@@ -6,22 +6,31 @@ const DisplayResults = ({ searchResults }) => {
 		searchResults && (
 			<div className="absolute bottom-[-1]">
 				<div className="w-[40vh] md:w-full max-h-[70vh] overflow-auto bg-[#0a192f] rounded-lg">
-					{searchResults.map((result) => (
-						<Link href={`/anime/${result.animeId}`}>
-							<div
-								className="grid grid-cols-[4fr_6fr] gap-3 place-content-center
+					{searchResults.length > 0 ? (
+						searchResults.map((result) => (
+							<Link href={`/anime/${result.animeId}`}>
+								<div
+									className="grid grid-cols-[4fr_6fr] gap-3 place-content-center
                place-items-center py-2 cursor-pointer hover:bg-[#0f2647] duration-200"
-							>
-								<Image
-									height={160}
-									width={120}
-									src={result.animeImg}
-									className="rounded-lg"
-								/>
-								<p className="text-slate-200">{result.animeTitle}</p>
-							</div>
-						</Link>
-					))}
+								>
+									<Image
+										height={160}
+										width={120}
+										src={result.animeImg}
+										className="rounded-lg"
+									/>
+									<p className="text-slate-200">{result.animeTitle}</p>
+								</div>
+							</Link>
+						))
+					) : (
+						<div
+							className="grid place-content-center
+               place-items-center h-24 w-full p-4"
+						>
+							<p className="text-slate-200">No Anime Found</p>
+						</div>
+					)}
 				</div>
 			</div>
 		)
