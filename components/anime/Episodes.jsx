fd774619010beca46100d80a30c9edf7409d7c57
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-13 07:56:23 +0800

    display info when there is no episode

--- components/anime/Episodes.jsx
+++ components/anime/Episodes.jsx
@@ -5,7 +5,7 @@ const Episodes = ({ animeData, setCurrentEpisode }) => {
 		<div className="text-slate-200 text-left mt-3">
 			<h1 className="text-3xl font-semibold mb-3">Episodes List</h1>
 			<div className="py-0 bg overflow-y-auto overflow-x-hidden max-h-[70vh]">
-				{animeData.episodesList &&
+				{animeData.episodesList.length > 0 ? (
 					animeData.episodesList.reverse().map((episode) => {
 						return (
 							<div
@@ -25,7 +25,18 @@ const Episodes = ({ animeData, setCurrentEpisode }) => {
 								</span>
 							</div>
 						);
-					})}
+					})
+				) : (
+					<div
+						className="flex flex-row gap-2 w-full justify-center 
+							rounded-lg bg-[#183868] text-blue-300 my-2
+							hover:scale-105 transition-all duration-500"
+					>
+						<h4 className="text-lg font-normal py-2 ml-2 cursor-pointer">
+							No Episode
+						</h4>
+					</div>
+				)}
 			</div>
 		</div>
 	);
