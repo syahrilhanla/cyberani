ae1565637d33c28324afd5b82bdfc3d35775b4bb
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-13 08:47:11 +0800

    made Link wrap buton

--- components/anime/Episodes.jsx
+++ components/anime/Episodes.jsx
@@ -6,20 +6,18 @@ const Episodes = ({ animeData, setCurrentEpisode }) => {
 			<h1 className="text-3xl font-semibold mb-3">Episodes List</h1>
 			<div className="py-0 bg overflow-y-auto overflow-x-hidden max-h-[70vh]">
 				{animeData.episodesList.length > 0 ? (
-					animeData.episodesList.reverse().map((episode) => {
+					animeData.episodesList.map((episode) => {
 						return (
 							<div
 								className="flex flex-row gap-2 w-full justify-center 
 							rounded-lg bg-[#183868] text-blue-300 my-2
-							hover:scale-105 transition-all duration-500"
+							hover:scale-105 transition-all duration-500 cursor-pointer"
 								key={episode.episodeNum}
+								onClick={() => setCurrentEpisode(episode.episodeNum)}
 							>
 								<span className="flex flex-row justify-center items-center">
 									<FaPlay />
-									<h4
-										className="text-lg font-normal py-2 ml-2 cursor-pointer"
-										onClick={() => setCurrentEpisode(episode.episodeNum)}
-									>
+									<h4 className="text-lg font-normal py-2 ml-2">
 										Episode {episode.episodeNum}
 									</h4>
 								</span>
