800341872070ed4c7aec08c6d2acb996c749c991
syahrilhanla <syahrilhanla5@gmail.com>
2022-08-22 16:13:45 +0700

    adjusted display on anime details

--- components/anime/Episodes.jsx
+++ components/anime/Episodes.jsx
@@ -1,14 +1,15 @@
-import { FaPlay } from "react-icons/fa";
-import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
-import { goToEpisode, nextEpisode, prevEpisode } from "../redux/animeSlice";
 import { useDispatch } from "react-redux";
+import { goToEpisode, nextEpisode, prevEpisode } from "../redux/animeSlice";
+
 import GoToEpisodeForm from "../common/GoToEpisodeForm";
+
+import { FaPlay } from "react-icons/fa";
 
 const Episodes = ({ animeData }) => {
 	const dispatch = useDispatch();
 
 	return (
-		<div className="text-slate-200 text-left mt-3 max-h-[80vh]">
+		<div className="text-slate-200 text-left max-h-[80vh]">
 			<h1 className="text-3xl font-semibold mb-3">Episodes List</h1>
 			<div className="py-0 px-2 overflow-y-auto overflow-x-hidden h-full">
 				<GoToEpisodeForm animeData={animeData} />
@@ -24,7 +25,6 @@ const Episodes = ({ animeData }) => {
 						>
 							<span className="flex flex-row items-center">
 								<h4 className="text-lg font-normal py-2 ml-2">Next Episode</h4>
-								<AiOutlineRight />
 							</span>
 						</div>
 						<div
@@ -36,14 +36,13 @@ const Episodes = ({ animeData }) => {
 							}}
 						>
 							<span className="flex flex-row justify-center items-center">
-								<AiOutlineLeft />
 								<h4 className="text-lg font-normal py-2 ml-2">Prev Episode</h4>
 							</span>
 						</div>
 						{animeData.episodesList.map((episode) => {
 							return (
 								<div
-									className="flex flex-row gap-2 w-full justify-center 
+									className="flex flex-row gap-2 w-full justify-center
 							rounded-lg bg-[#183868] text-blue-300 my-2
 							hover:scale-105 transition-all duration-500 cursor-pointer"
 									key={episode.episodeNum}
@@ -51,7 +50,7 @@ const Episodes = ({ animeData }) => {
 										dispatch(goToEpisode(episode.episodeNum));
 									}}
 								>
-									<span className="flex flex-row justify-center items-center">
+									<span className="flex flex-row justify-start items-center">
 										<FaPlay />
 										<h4 className="text-lg font-normal py-2 ml-2">
 											Episode {episode.episodeNum}
