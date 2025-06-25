3b5a1eacbc5a39f993510d5c1b87d2ec5f76e1e6
syahrilhanla <syahrilhanla5@gmail.com>
2025-06-25 10:11:47 +0700

    feat: add MyAnimeList link and improve layout in AnimeInfo component

--- components/anime/AnimeInfo.jsx
+++ components/anime/AnimeInfo.jsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { useState } from "react";
 import { Link } from "react-scroll";
 
+import { SiMyanimelist } from "react-icons/si";
+
 const AnimeInfo = ({ animeData }) => {
 	const [expandDescription, setExpandDescription] = useState(false);
 
@@ -22,16 +24,24 @@ const AnimeInfo = ({ animeData }) => {
 			<div className="p-4 gap-3 grid grid-cols-1 w-full lg:w-auto">
 				<Link to="watch" smooth={true} duration={500}>
 					<button
-						className="lg:hidden px-4 py-2 bg-gradient-to-r from-blue-500 to-purple-500 text-white rounded-md
+						className="lg:hidden w-full px-4 py-2 bg-gradient-to-r from-blue-500 to-purple-500 text-white rounded-md
 							hover:scale-105 transition-transform duration-300 font-semibold shadow-md"
 					>
 						Watch Now
 					</button>
 				</Link>
 
-				<h1 className="text-lg font-bold text-blue-300">
-					Title: {animeData.title}
-				</h1>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href={`https://myanimelist.net/anime/${animeData.malID}`}
+					className="text-sm px-4 py-2 md:-mt-3 flex gap-2 items-center justify-center bg-gradient-to-r from-blue-800/10 to-blue-900 text-white rounded-md
+							hover:scale-105 transition-transform duration-300 font-semibold shadow-md"
+				>
+					<SiMyanimelist size={28} /> More Info
+				</a>
+
+				<h1 className="text-lg font-bold text-blue-300">{animeData.title}</h1>
 				{animeData.japaneseTitle && (
 					<h2 className="text-sm font-medium text-blue-200">
 						Other Names: {animeData.japaneseTitle}
@@ -44,7 +54,7 @@ const AnimeInfo = ({ animeData }) => {
 					</span>
 					{animeData.description && animeData.description.length > 120 && (
 						<button
-							className="ml-2 text-blue-400 hover:underline focus:outline-none text-xs"
+							className="text-blue-400 hover:underline focus:outline-none text-xs"
 							onClick={() => setExpandDescription((prev) => !prev)}
 						>
 							{expandDescription ? "Show less" : "Read more"}
@@ -52,22 +62,23 @@ const AnimeInfo = ({ animeData }) => {
 					)}
 				</h2>
 				<h2 className="text-sm font-medium text-blue-200">
-					Type: {animeData.type}
+					<span className="font-normal">Type</span>: {animeData.type}
 				</h2>
 				<h2 className="text-sm font-medium text-blue-200">
-					Status: {animeData.status}
+					<span className="font-normal">Status</span>: {animeData.status}
 				</h2>
 				<h2 className="text-sm font-medium text-blue-200">
-					Season: {animeData.season}
+					<span className="font-normal">Season</span>: {animeData.season}
 				</h2>
 				{animeData.type !== "Movie" && (
 					<h2 className="text-sm font-medium text-blue-200">
-						Total Episodes: {animeData.totalEpisodes}
+						<span className="font-normal">Total Episodes</span>:{" "}
+						{animeData.totalEpisodes}
 					</h2>
 				)}
 				{animeData.genres && (
 					<div className="flex flex-wrap gap-2">
-						<h2 className="text-sm font-medium text-blue-200">Genres:</h2>
+						<h2 className="text-sm font-normal text-blue-200">Genres:</h2>
 						{animeData.genres.map((genre) => (
 							<span
 								key={genre}
