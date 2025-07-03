a84a52d2eb2c009d5774f4639615e968b32d0ac6
syahrilhanla <syahrilhanla5@gmail.com>
2025-07-03 19:40:42 +0700

    feat: add skeleton loading to home suggestion

--- components/homepage/Suggestion.jsx
+++ components/homepage/Suggestion.jsx
@@ -9,7 +9,15 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import { fetchAnimeList } from "../utils/fetchAnime";
-import { BiLoaderAlt } from "react-icons/bi";
+
+const SkeletonJumbotron = () => (
+	<div className="h-full w-full bg-gradient-to-r from-slate-800 to-slate-700 animate-pulse rounded-lg flex flex-col justify-center items-start p-6">
+		<div className="h-10 w-1/3 bg-slate-600 rounded mb-4"></div>
+		<div className="h-6 w-1/3 bg-slate-600 rounded mb-2"></div>
+		<div className="h-4 w-1/3 bg-slate-600 rounded mb-6"></div>
+		<div className="h-10 w-32 bg-slate-600 rounded-full"></div>
+	</div>
+);
 
 const Suggestion = () => {
 	const [topAiringList, setTopAiringList] = useState([]);
@@ -44,8 +52,8 @@ const Suggestion = () => {
 	return (
 		<div className="w-[95%] mb-7">
 			{loading ? (
-				<div className="flex justify-center items-center h-64 text-slate-200">
-					<BiLoaderAlt size={64} color="slate" className="animate-spin" />
+				<div className="flex justify-center items-center h-[20dvh] lg:h-[30dvh] xl:h-[50dvh] text-slate-200">
+					<SkeletonJumbotron />
 				</div>
 			) : (
 				<Swiper
